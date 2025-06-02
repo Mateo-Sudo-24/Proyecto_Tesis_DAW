baad36422691fccfc264d7c5226747c00e87400b
Mateo-Sudo-24 <mateo.paredes@epn.edu.ec>
2025-06-01 20:26:04 -0500

    Delete src directory

--- src/routers/Administrador_routers.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-import { registro } from '../controllers/Administrador_controller.js'
-const router = Router()
-
-
-router.post('/registro',registro)
-
-
-export default router
