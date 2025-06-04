5ab3300275ebc47d1644f58312a063851b3bdeb7
KZLenin <leninmateo4@gmail.com>
2025-06-04 00:26:46 -0500

    Update App.tsx

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import "./global.css"
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/SignupScreen"
 const Stack = createNativeStackNavigator();
